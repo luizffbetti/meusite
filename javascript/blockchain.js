27882c31e52e50eda3ea10eefacfad31a98ad959
Luiz Fernando F Betti <56132219+luizffbetti@users.noreply.github.com>
2019-11-21 19:40:47 -0300

    Add files via upload

--- javascript/blockchain.js
+++ javascript/blockchain.js
@@ -1,4 +1,4 @@
-var enderecoContrato = "0x53C34f4897a4111f9299a9b734aD1d5c380a8Ce5";
+var enderecoContrato = "0x0e7B10f650E59Ce5671D8F94Db2606CAda92BB0f";
 var provedor = new ethers.providers.Web3Provider(web3.currentProvider);
 ethereum.enable();
 var signatario = provedor.getSigner();
