26ea73159ff991b36e4f4a5a8c20088c0b0d6974
Luiz Fernando F Betti <56132219+luizffbetti@users.noreply.github.com>
2019-11-26 21:46:05 -0300

    Add files via upload

---
+++ blockchain.js
@@ -0,0 +1,77 @@
+var enderecoContrato = "0x0e7B10f650E59Ce5671D8F94Db2606CAda92BB0f";
+var provedor = new ethers.providers.Web3Provider(web3.currentProvider);
+ethereum.enable();
+var signatario = provedor.getSigner();
+var contrato = new ethers.Contract(enderecoContrato, abiContrato, signatario);
+
+function registrarMudancaStatus() {
+    var textoCampo = document.frmStatus.txtStatusPagamentoAluguel.value;
+    var caixaStatusTx = document.getElementById("caixaStatusTx");
+    if (textoCampo.length === 8) {
+        caixaStatusTx.innerHTML = "Enviando transação...";
+        contrato.mudaStatusPagamento(textoCampo)
+        .then( (transacao) => {
+            console.log("registrarMudancaStatus - Transacao ", transacao);   
+            caixaStatusTx.innerHTML = "Transação enviada. Aguardando processamento...";
+            transacao.wait()
+            .then( (resultado) => {
+                buscaStatusContrato();
+                caixaStatusTx.innerHTML = "Transação realizada.";
+            })        
+            .catch( (err) => {
+                console.error("registrarMudancaStatus - Aguardando tx ser minerada");
+                console.error(err);
+                caixaStatusTx.innerHTML = "Algo saiu errado: " + err.message;
+            })
+        })
+        .catch( (err) => {
+            console.error("registrarMudancaStatus");
+            console.error(err);
+            caixaStatusTx.innerHTML = "Algo saiu errado: " + err.message;
+        })
+    }
+}
+
+function encerrarContrato()
+{
+    var textoEncerrar = document.getElementById("encerrarContratoTx");
+
+    textoEncerrar.innerHTML="conectando para encerramento de contrato ...";
+    contrato.fimDoContrato()
+    .then( (transacao)=>
+    {
+            console.log("encerrarContrato - Transacao", transacao);
+            textoEncerrar.innerHTML="aguarde encerrando o contrato ...";
+        
+            transacao.wait()
+            .then((resultado)=>
+            {
+                buscaFimContrato();
+            })
+            .catch((err) =>
+            {
+                console.error("encerrarContrato - Aguardando tx ser minerada");
+                console.error(err);
+                textoEncerrar.innerHTML="erro ao se conectar ...";
+            })
+     })
+     .catch((err)=>
+     {
+            console.error("encerrarContrato - Aguardando tx ser minerada");
+            console.error(err);
+            textoEncerrar.innerHTML="erro ao se conectar ...";
+     })
+}
+
+function buscaStatusContrato() {
+    var status;
+    var campoStatus = document.getElementById("campoStatus");     
+    contrato.statusPagamentoAluguel()
+    .then( (resultado) => {
+        campoStatus.innerHTML = resultado;
+    })
+    .catch( (err) => {
+        console.error(err);
+        campoStatus.innerHTML = err;
+    });
+}
